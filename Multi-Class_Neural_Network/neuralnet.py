575bbf07f29aab4f3b501b74b77f0c0b2c7adaa0
Erick <Erick.Cabrera64@myhunter.cuny.edu>
2022-09-22 08:54:20 -0400

    Generalized Neural Net to work for any input nodes.

--- Multi-Class_Neural_Network/neuralnet.py
+++ Multi-Class_Neural_Network/neuralnet.py
@@ -4,33 +4,43 @@ import csv
 import random
 import copy
 from sklearn.utils import shuffle
+import string
 
 class Sequential():
 
     def initial_deltas(self):
 
-        fw = np.zeros((533,784)) # change these to its own function called init.
-        fb = np.zeros((533,1))
-        fw1 = np.zeros((533,10))
-        fb1 = np.zeros((10,1))
-        aw,ab,aw1,ab1 = 0,0,0,0
-    
-        return fw,fb,fw1,fb1,aw,ab,aw1,ab1
+        pw = np.zeros((533,784)) # change these to its own function called init.
+        pb = np.zeros((533,1))
+        pw1 = np.zeros((533,10))
+        pb1 = np.zeros((10,1))
+        aw,ab,aw1,ab1,t = 0,0,0,0,0
 
-    def weightsandbiases(self):
+        return pw,pb,pw1,pb1,aw,ab,aw1,ab1,t
 
-        np.random.seed (0)  # size(sets, nodes)
-        w = np.random.uniform(size=(533,784),low = -1, high= 1) * np.sqrt(2 / 784) #maybe change to -1 as low 
-        np.random.seed(0)
-        b = np.random.uniform(size=(533,1),low = 0, high= 1) * 0   # very small biases #b = np.random.uniform(size=(533,1),low = -1, high= 1) * np.sqrt(2 / 533) 
+    def weightsandbiases(self,layernodes):
 
-        ############################################################################################
-        np.random.seed (1)
-        w1 = np.random.uniform(size=(533,10),low = -1, high= 1) * np.sqrt(2 / 533)#maybe change to -1 as low 
-        np.random.seed(1)
-        b1 = np.random.uniform(size=(10,1),low = 0, high= 1) * 0  # very small biases # b1 = np.random.uniform(size=(10,1),low = -1, high= 1) *  np.sqrt(2 / 10) 
-        
-        return w,b,w1,b1
+        #print (layernodes) # 0,1 #784,533,10 # 1,2
+        dynamicwandb = ()
+        for i in range (0,len(layernodes)-1):
+            np.random.seed (i)  # size(sets, nodes)
+            w = np.random.uniform(size=(layernodes[i+1],layernodes[i]),low = -1, high= 1) * np.sqrt(2 / layernodes[i]) #maybe change to -1 as low 
+            np.random.seed(i)
+            b = np.random.uniform(size=(layernodes[i+1],1),low = 0, high= 1) * 0   # very small biases #b = np.random.uniform(size=(533,1),low = -1, high= 1) * np.sqrt(2 / 533) 
+            dynamicwandb += (w,b,)            
+                #np.random.seed (0)  # size(sets, nodes)
+                #w = np.random.uniform(size=(layernodes[1],layernodes[0]),low = -1, high= 1) * np.sqrt(2 / layernodes[0]) #maybe change to -1 as low 
+                #np.random.seed(0)
+                #b = np.random.uniform(size=(layernodes[1],1),low = 0, high= 1) * 0   # very small biases #b = np.random.uniform(size=(533,1),low = -1, high= 1) * np.sqrt(2 / 533) 
+                ##################################################################################################################
+                #np.random.seed (1)
+                #w1 = np.random.uniform(size=(layernodes[2],layernodes[1]),low = -1, high= 1) * np.sqrt(2 / layernodes[1])#maybe change to -1 as low 
+                #np.random.seed(1)
+                #print (w1)
+                #b1 = np.random.uniform(size=(layernodes[2],1),low = 0, high= 1) * 0  # very small biases # b1 = np.random.uniform(size=(10,1),low = -1, high= 1) *  np.sqrt(2 / 10) 
+                #return w,b,w1,b1
+
+        return dynamicwandb
 
     def shuffledtrain(self):
                 
@@ -41,7 +51,7 @@ class Sequential():
             writer = csv.writer(f)
             writer.writerows(shuffleddata)
 
-    def ImageArray(self, rnumber): # replaces with one picture from real array 
+    def ImageArray(self): # replaces with one picture from real array 
         
         numpyarray = np.array([])
 
@@ -49,26 +59,27 @@ class Sequential():
             csvreader = csv.reader(csv_file) # loads data into csv reader
             #next(csvreader) # not needed for shuffled only for train # skips pixelnumbers skips 0 
             for index, row in enumerate(csvreader): # enumerates file into indexes
-               if index==rnumber:  # Load image at row given
+               if index==k:  # Load image at row given
                     pixels = row[1:] # for train.csv ignores or skips over the 0th column  
                     self.label = row[0] #! These are the labels of train.csv 
             
             numpyarray = np.append (numpyarray, list(map(float, pixels)) ) # map characthers as integers
-            self.scaledarray =  numpyarray/255
+            self.scaledarray = numpyarray/255
         return self.scaledarray # full image is established in nested list. Divided by 255 to get value under 1.
 
-    def Linearl0tol1 (self, inductweights, inductbiases):
-        arrayofpixels = copy.deepcopy(self.scaledarray) # this is essential as it was getting pdrelu instead
-        npinsertarr = arrayofpixels.reshape(784,1) # reduced this was next line : npinsertarr = npinsertarr.reshape(784,1)
-        #! change mutliplication to dot because it isnt supposed to just get multiplied 
-        weightwithbias = np.dot (inductweights, npinsertarr) + inductbiases # maybe inductweights @ npinsertarr instead of dot. 
-        self.inductivebeforerelu = weightwithbias.reshape(533) #.tolist()
+    
+    def Linear(self,layer2nodes,layer3nodes,arraymapped,w_,b_):
 
-        #print ("Actual:  ",self.inductivebeforerelu)
-        return self.inductivebeforerelu
+        array = copy.deepcopy(arraymapped) # this is essential as it was getting pdrelu instead
+        npinsertarr = np.array(array).reshape(layer2nodes,1) # reduced this was next line : npinsertarr = npinsertarr.reshape(784,1)       
+        self.dotprod = (np.dot (w_, npinsertarr) ) + b_
+        self.dotprod= self.dotprod.reshape(layer3nodes)
+        
+        return self.dotprod
 
-    def LeakyRelU(self,VALUE): # perhaps I pass the prev output in here
-        array = copy.deepcopy(self.inductivebeforerelu) # this is essential as it was getting pdrelu instead
+
+    def LeakyRelU(self,VALUE, First_Layer): # perhaps I pass the prev output in here
+        array = copy.deepcopy(First_Layer) # this is essential as it was getting pdrelu instead
         self.output= np.maximum(VALUE,array)
         #!print (f'Relu {self.output}')
         return self.output
@@ -83,21 +94,9 @@ class Sequential():
 
         return self.derivrelu
     
-    def Linearl1tol2 (self, inductweightsl1, inductbiasesl1):
-        
-        array = copy.deepcopy(self.output) # this is essential as it was getting pdrelu instead
-        #print (self.output)
-        npinsertarr = np.array(array).reshape(533,1) # reduced this was next line : npinsertarr = npinsertarr.reshape(784,1)       
-        inductweightsl1 = inductweightsl1.reshape (10,533) #! added
-        self.inductiveAFTERRelu = (np.dot (inductweightsl1, npinsertarr) ) + inductbiasesl1
-        #inductiveAFTERRelu = inductiveAFTERRelu#.reshape(10)
-        #print (f'inductiveAFTERRelu::{inductiveAFTERRelu}' )
-
-        return self.inductiveAFTERRelu
-
-    def Softmax(self): #! still needs fixing  
+    def Softmax(self,Second_Layer): #! still needs fixing  
         # ! The problem is the weights.It was learning to fast thus a smaller learning rate
-        array = copy.deepcopy(self.inductiveAFTERRelu) # this is essential as it was getting pdrelu instead
+        array = copy.deepcopy(Second_Layer) # this is essential as it was getting pdrelu instead
         self.softmaxlist =  np.exp(array) / sum(np.exp(array))
         self.softmaxlist = self.softmaxlist.reshape(1,10)[0] 
         #!print (f'Softmax: {self.softmaxlist}')
@@ -147,8 +146,8 @@ class Sequential():
         self.crossentropyarray = np.sum(-nphotencode*np.log(array))
         print (f"CROSS ENTROPY: {self.crossentropyarray}")
         return self.crossentropyarray 
-    
-    def Probability(self):  # pred = something new , y = label
+
+    def Score(self,amtcorrect):  # pred = something new , y = label
     
         correctlabel_ = copy.deepcopy(self.label) # this is essential as it was getting pdrelu instead        
         sftarray = copy.deepcopy(self.softmaxlist) # this is essential as it was getting pdrelu instead
@@ -164,7 +163,11 @@ class Sequential():
         
         print (f'\nCORRECT LABEL::: {Y_}  PREDICTED LABEL:::  {YPred_}  Probability Of Correct Label :::  {correctpred_ * 100} %\n')
 
-        return (Y_==YPred_)#,Y_, YPred_, correctpred_ # cross entropy, if they are equal, 
+        if ((Y_==YPred_) == True ):
+            amtcorrect+=1
+            print (f'          TRUE: K: {k} Score: {amtcorrect/Images * 100}%   \n') 
+            
+        return amtcorrect#,Y_, YPred_, correctpred_ # cross entropy, if they are equal, 
 
     def D_CCELoss(self):
         correctlabel_ = copy.deepcopy(self.label) # this is essential as it was getting pdrelu instead        
@@ -191,36 +194,37 @@ class Sequential():
         self.loss = crossder_ @ softpartials_ 
         return self.loss 
 
-    def Backward_Prop(self,ogweightw1,ogweight,ogbiasb1,kth):
-
+    def Backward_Prop(self,layernodes,ogweightw1,ogweight,ogbiasb1):
+        
         pixels = copy.deepcopy(self.scaledarray) # this is essential as it was getting pdrelu instead
         reludone = copy.deepcopy(self.output) # this is essential as it was getting pdrelu instead
         loss = copy.deepcopy(self.loss) # this is essential as it was getting pdrelu instead
         drelu = copy.deepcopy(self.derivrelu) # this is essential as it was getting pdrelu instead
         
-        w1loss  = np.dot (loss.reshape(10,1), reludone.reshape(1,533)) * 1/(kth + 1) # # 10 *   10,10  * 10  =  10,10 
-        nw1 = w1loss.reshape(533,10)
-        #################### everythings good. I am a bit concerned about bias...####################
-        nb1 = np.sum(nw1.reshape(10,533),1).reshape(10,1) * 1/(kth+1)  #loss.reshape(10,1)* 1/(kth+1) 
+        w1loss  = np.dot (loss.reshape(layernodes[2],1), reludone.reshape(1,layernodes[1])) * 1/(k + 1) # # 10 *   10,10  * 10  =  10,10 
+        nw1 = w1loss.reshape(layernodes[2],layernodes[1])
+        nb1 = np.sum(nw1,1).reshape(layernodes[2],1) * 1/(k+1)  #loss.reshape(10,1)* 1/(kth+1) 
 
-        wloss = np.dot (loss.reshape(1,10),ogweightw1.reshape(10,533)) * drelu   
-        nw = np.dot (wloss.reshape(533,1), pixels.reshape(1,784)) * 1/(kth + 1)
-        nb = np.sum(nw.reshape(533,784),1).reshape(533,1) * 1/(kth+1) 
+        wloss = np.dot (loss.reshape(1,layernodes[2]),ogweightw1.reshape(layernodes[2],layernodes[1])) * drelu   
+        nw = np.dot (wloss.reshape(layernodes[1],1), pixels.reshape(1,layernodes[0])) * 1/(k + 1)
+        nb = np.sum(nw.reshape(layernodes[1],layernodes[0]),1).reshape(layernodes[1],1) * 1/(k+1) 
+
     
         return nw,nb,nw1,nb1 # change these to self after adding momentum 
 
-    def GradientDescentWithMomentum(self,w,b,w1,b1,nw,nb,nw1,nb1,prevnw,prevnb,prevnw1,prevnb1,alterdw,alterdb,alterdw1,alterdb1,kth,mu,lr):
+    def GradientDescentWithMomentum(self,w,b,w1,b1,nw,nb,nw1,nb1,prevnw,prevnb,prevnw1,prevnb1,alterdw,alterdb,alterdw1,alterdb1,mu,lr):
          
         deltaw = (lr * nw) 
         deltab = (lr * nb) 
         deltaw1 = (lr * nw1) 
         deltab1 = (lr * nb1) 
-
-        if kth==0:        
+        
+        if k==0:        
             neww = w - deltaw
             newb = b - deltab    #b - (nb * learnrate)
             neww1 = w1 - deltaw1 #w1 - (nw1 * learnrate) 
             newb1 = b1 - deltab1 #b1 - (nb1 * learnrate)
+
             alterdw = neww
             alterdb = newb
             alterdw1 = neww1
@@ -239,49 +243,44 @@ class Sequential():
 
         return neww,newb,neww1,newb1,alterdw,alterdb,alterdw1,alterdb1 
 
-    
 if __name__ == "__main__":
-
-    ######### Initalizing data  #################
-    accuratepredictions = 0
-    images = 100 #29400 # training at 80 percent for 29,400 images. 
-
+    
     ################# Model #####################
-    nn = Sequential()  # calls class epoch cycle
+    nn = Sequential()  
+    pw,pb,pw1,pb1,aw,ab,aw1,ab1,t = nn.initial_deltas()
+    
+    Nodes = (784,533,10) # Enter node layers here
+    w,b,w1,b1 = nn.weightsandbiases(Nodes)
 
-    fw,fb,fw1,fb1,aw,ab,aw1,ab1  = nn.initial_deltas()
-    w,b,w1,b1 = nn.weightsandbiases()
+    Images = 10 #29400 # training at 81 percent for 29,400 images. 
+    Momentum = 0.9
+    Learning_Rate = 0.1
 
-    for k in range(0,images): #trainingset:  # loops through images. 90 sec = 10 images image 0 and forward 
-
-        nn.ImageArray(k) #(3)# read all 42000. For loop. Gets data from index 4         
-        
+    for k in range(0,Images): #trainingset:  # loops through images. 90 sec = 10 images image 0 and forward 
+        imgs_ = nn.ImageArray() # K is passed through all of these functions.     
         ########################### Starting Forward Prop ###########################
-        nn.Linearl0tol1(w,b)  
-        nn.LeakyRelU(0.01)  
-        nn.D_LeakyRelU(0.01) 
-        nn.Linearl1tol2(w1,b1) 
-        nn.Softmax() 
-
+        L1 = nn.Linear(Nodes[0],Nodes[1],imgs_,w,b)
+        LRelu = nn.LeakyRelU(L1,0.01)  
+        nn.D_LeakyRelU(0.01)
+        
+        L2 = nn.Linear(Nodes[1],Nodes[2],LRelu,w1,b1)
+        nn.Softmax(L2) 
         ######################### Starting Backward Prop #########################
         nn.D_Softmax() # SOFTMAX partial derivatives or gradients
         nn.CCELoss() # array,labeltarget # Cross entropy on SOFTMAX  
-        
-        if nn.Probability() == True:
-            print (f' ACCURATE == PREDICTED  TRUE   K: {k}   \n') 
-            accuratepredictions+=1
-
+        t = nn.Score(t)
         nn.D_CCELoss()  # array,labeltarget # Cross entropy derivative 
         nn.D_CCE_and_Softmax() # Cross entropy & SOFTMAX Chain Rule derivative
 
         if k!=0:
-            fw,fb,fw1,fb1 = nw,nb,nw1,nb1 
+            pw,pb,pw1,pb1 = nw,nb,nw1,nb1 
         
-        nw,nb,nw1,nb1 = nn.Backward_Prop(w1,w,b1,k)
-        w,b,w1,b1,aw,ab,aw1,ab1 = nn.GradientDescentWithMomentum(w,b,w1,b1,nw,nb,nw1,nb1,fw,fb,fw1,fb1,aw,ab,aw1,ab1,k,0.9, 0.1)
+        nw,nb,nw1,nb1 = nn.Backward_Prop(Nodes,w1,w,b1)
+        w,b,w1,b1,aw,ab,aw1,ab1 = nn.GradientDescentWithMomentum(w,b,w1,b1,nw,nb,nw1,nb1,pw,pb,pw1,pb1,aw,ab,aw1,ab1,Momentum,Learning_Rate)
 
-    accuracy = accuratepredictions / images # len (trainingset) # change to len (testingset) when running testing set.    
+    accuracy = t / Images # len (trainingset) # change to len (testingset) when running testing set.    
     print (f'\n ACCURACY  ::: {accuracy * 100}%')
+
     # NEW ONE : 0.01 YEILDS 24 PERCENT FOR 100.
     # NEW ONE : 0.09 YEILDS 33 PERCENT FOR 100.
     # NEW ONE : 0.10 YEILDS 36 PERCENT FOR 100. For 29,400 it yeilds 80.227%
