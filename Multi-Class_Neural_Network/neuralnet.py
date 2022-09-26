d5854547324c90df662fc6080ce011d23b535056
Erick <Erick.Cabrera64@myhunter.cuny.edu>
2022-09-26 06:36:16 -0400

    Memory efficnet cost

--- Multi-Class_Neural_Network/neuralnet.py
+++ Multi-Class_Neural_Network/neuralnet.py
@@ -17,7 +17,6 @@ class Sequential():
         return repeat
 
     def weightsandbiases(self):
-
         dynamicwandb = []
         for i in range (0,len(Nodes)-1):
             np.random.seed (i)  # size(sets, nodes)
@@ -25,7 +24,6 @@ class Sequential():
             np.random.seed(i)
             b = np.random.uniform(size=(Nodes[i+1],1),low = 0, high= 1) * 0   # very small biases #b = np.random.uniform(size=(533,1),low = -1, high= 1) * np.sqrt(2 / 533) 
             dynamicwandb += [w,b,]  
-        #print (dynamicwandb)
         return dynamicwandb
 
     def shuffledtrain(self):
@@ -53,131 +51,108 @@ class Sequential():
             self.scaledarray = numpyarray/255
         return self.scaledarray # full image is established in nested list. Divided by 255 to get value under 1.
 
-    
     def Linear(self,layer2nodes,layer3nodes,arraymapped,w_,b_):
-
-        array = copy.deepcopy(arraymapped) # this is essential as it was getting pdrelu instead
-        npinsertarr = np.array(array).reshape(layer2nodes,1) # reduced this was next line : npinsertarr = npinsertarr.reshape(784,1)       
+        
+        npinsertarr = np.array(arraymapped).reshape(layer2nodes,1) # reduced this was next line : npinsertarr = npinsertarr.reshape(784,1)       
         dotprod = (np.dot (w_, npinsertarr) ) + b_
-        dotprod= dotprod.reshape(layer3nodes)
+        dotprod = dotprod.reshape(layer3nodes)
         
         return dotprod
 
-    def LeakyRelU(self,VALUE, First_Layer): # perhaps I pass the prev output in here
-        array = copy.deepcopy(First_Layer) # this is essential as it was getting pdrelu instead
-        self.output= np.maximum(VALUE,array)
-        return self.output
-    
-    def D_LeakyRelU(self,VALUE):
-        copyofrelu = copy.deepcopy(self.output) # this is essential as it was getting pdrelu instead
-        copyofrelu[copyofrelu<=VALUE] = 0
-        copyofrelu[copyofrelu>0] = 1
-        self.derivrelu = copyofrelu
+    def LeakyRelU(self,First_Layer,VALUE): # perhaps I pass the prev output in here 
+        return np.maximum(VALUE,First_Layer)
 
-        #!print (f'self.derivrelu{self.derivrelu}')
-        return self.derivrelu
+    def D_LeakyRelU(self,PREVLEAKY,VALUE):
+        return np.greater(PREVLEAKY, VALUE).astype(int)
     
     def Softmax(self,Second_Layer): #! still needs fixing  
-        array = copy.deepcopy(Second_Layer) # this is essential as it was getting pdrelu instead
-        self.softmaxlist =  np.exp(array) / sum(np.exp(array))
+
+        self.softmaxlist =  np.exp(Second_Layer) / sum(np.exp(Second_Layer))
         self.softmaxlist = self.softmaxlist.reshape(1,10)[0] 
         return self.softmaxlist
 
     def D_Softmax(self):
-        array = copy.deepcopy(self.softmaxlist) # this is essential as it was getting pdrelu instead
-        soft = array.reshape(10,1)#,1)
+        soft = self.softmaxlist.reshape(10,1)
         self.partials = np.diagflat(soft) - np.dot(soft, soft.T)
         return self.partials
 
     def Hotencode (self, desired):
         return np.eye(10,dtype="float")[desired] 
 
-    def CCELoss(self) :
-        print (self.label)
-        correctlabel_ = copy.deepcopy(self.label) # this is essential as it was getting pdrelu instead        
-        array = copy.deepcopy(self.softmaxlist) # this is essential as it was getting pdrelu instead
-        nphotencode = self.Hotencode( int (correctlabel_)) #self.Hotencode(intlabel).reshape(10)  
-        self.crossentropyarray = np.sum(-nphotencode*np.log(array))
+    def CCELoss(self) :       
+        nphotencode = self.Hotencode( int (self.label)) #self.Hotencode(intlabel).reshape(10)  
+        self.crossentropyarray = np.sum(-nphotencode*np.log(self.softmaxlist))
         print (f"CROSS ENTROPY: {self.crossentropyarray}")
         return self.crossentropyarray 
 
     def Score(self,amtcorrect):  # pred = something new , y = label
     
-        correctlabel_ = copy.deepcopy(self.label) # this is essential as it was getting pdrelu instead        
-        sftarray = copy.deepcopy(self.softmaxlist) # this is essential as it was getting pdrelu instead
-        predlabel = np.argmax(sftarray)
-        correctpred_= 0 
+        predlabel = np.argmax(self.softmaxlist)
+        percentpred_= 0 
         
         for i in range (10):
-            if i==int (correctlabel_):
-                correctpred_ = sftarray[i]
-        
-        print (f'\nCORRECT LABEL::: {int (correctlabel_)}  PREDICTED LABEL:::  {int (predlabel)}  Probability Of Correct Label :::  {correctpred_ * 100} %\n')
+            if i == int(self.label):
+                percentpred_ = self.softmaxlist[i]
 
-        if ((int (correctlabel_)==int (predlabel)) == True ):
+        print (f'\nCORRECT LABEL::: {int (self.label)}  PREDICTED LABEL:::  {int (predlabel)}  Probability Of Correct Label :::  {percentpred_ * 100} %\n')
+
+        if ((int (self.label)==int (predlabel)) == True ):
             amtcorrect+=1
             print (f'          TRUE: K: {k} Score: {amtcorrect/Images * 100}%   \n') 
             
-        return amtcorrect#,Y_, YPred_, correctpred_ # cross entropy, if they are equal, 
+        return amtcorrect
 
     def D_CCELoss(self):
-        correctlabel_ = copy.deepcopy(self.label) # this is essential as it was getting pdrelu instead        
-        softmax = copy.deepcopy(self.softmaxlist) # this is essential as it was getting pdrelu instead
+
         self.crossder = np.array ([])        
-        targethotencode = self.Hotencode(int(correctlabel_)) #.reshape(10).tolist() 
-        
+        targethotencode = self.Hotencode(int(self.label)) #.reshape(10).tolist() 
         infinitesmal = float ('-inf')
-        undefined = float ('nan')    
+        undefined = float ('nan')   
 
         for i in range (10):
-            #print (f"softmax[i]{softmax[i]}") # check softmax values.if they are all 0 and one of them is 1.00 then its wrong delete that iteration
-            if softmax[i] == 0.00000000e+000 or -float(targethotencode[i])/float (softmax[i]) == infinitesmal or -float(targethotencode[i])/float (softmax[i])==undefined or  -float(targethotencode[i])/float (softmax[i]) == -0. : # softmax[i] == 0.00000000e+000 or  ##reduces runtime error issue                
+            DCCE = -float(targethotencode[i])/float (self.softmaxlist[i])
+            if DCCE == (infinitesmal or undefined or -0.) : # 0.00000000e+000or  ##reduces runtime error issue                
                 self.crossder = np.append (self.crossder, 0.00)
             else:
-                self.crossder = np.append(self.crossder, -float(targethotencode[i])/float (softmax[i]))
-
-        #print (f'self.crossder{self.crossder}' )
+                self.crossder = np.append(self.crossder, -float(targethotencode[i])/float (self.softmaxlist[i]))
+        
         return self.crossder
 
     def D_CCE_and_Softmax (self):
-        crossder_ = copy.deepcopy(self.crossder) # this is essential as it was getting pdrelu instead
-        softpartials_ = copy.deepcopy(self.partials) # this is essential as it was getting pdrelu instead
-        self.loss = crossder_ @ softpartials_ 
-        
+        self.loss = self.crossder @ self.partials 
         return self.loss 
 
-    def Leaky_Relu_BackProp(self,drelu2_,relu1_,chainloss_,ogweightw2_): # Generalize so you can then add dynamically
+    def Leaky_Relu_BackProp(self,drelu2_,relu2_): # Generalize so you can then add dynamically
 
-        relu1 = copy.deepcopy(relu1_) # this is essential as it was getting pdrelu instead
-        chainloss = copy.deepcopy(drelu2_) # this is essential as it was getting pdrelu instead
-        drelu2 = copy.deepcopy(ogweightw2_) # this is essential as it was getting pdrelu instead
-        # Relu2 to Relu1
-        wloss2 = np.dot (chainloss.reshape(1,Nodes[2]), ogweightw2.reshape(Nodes[2],Nodes[1])) * drelu2           
-        nw2 = np.dot (wloss2.reshape(Nodes[1],1), relu1.reshape(1,Nodes[0])) * 1/(k+1)
-        nb2 = np.sum(nw.reshape(Nodes[1],Nodes[0]),1).reshape(Nodes[1],1) * 1/(k+1)                
-        return nw2,nb2
+        relu2 = copy.deepcopy(relu2_) # this is essential as it was getting pdrelu instead
+        drelu = copy.deepcopy(drelu2_) # this is essential as it was getting pdrelu instead
+        
+        # Relu2 to Relu1 0 ,2,4
+        #self.loss = 10 , 
+        wloss = np.dot (self.loss, WandB[2].reshape(Nodes[2],Nodes[1])) * drelu           
+        self.neww = np.dot (wloss.reshape(Nodes[1],1), relu2.reshape(1,Nodes[0])) * 1/(k+1)
+        self.newb = np.sum(self.neww.reshape(Nodes[1], Nodes[0]),1).reshape(Nodes[1],1) * 1/(k+1) 
+                      
+        return self.neww,self.newb
 
-    def Backward_Prop(self):
+    def Backward_Prop(self,PREVLEAKY,DL1Relu):
 
-        pixels = copy.deepcopy(self.scaledarray) # this is essential as it was getting pdrelu instead
-        reludone = copy.deepcopy(self.output) # this is essential as it was getting pdrelu instead
-        Chain = copy.deepcopy(self.loss) # this is essential as it was getting pdrelu instead
-        drelu = copy.deepcopy(self.derivrelu) # this is essential as it was getting pdrelu instead
+        #reludone = copy.deepcopy(PREVLEAKY) # this is essential as it was getting pdrelu instead
+        #drelu = copy.deepcopy(DL1Relu) # this is essential as it was getting pdrelu instead
         
         # Softmax to Relu 
-        w1loss  = np.dot (Chain.reshape(Nodes[2],1), reludone.reshape(1,Nodes[1])) 
-        self.nw1 = w1loss.reshape(Nodes[2],Nodes[1]) * 1/(k+1)  
-        self.nb1 = np.sum(self.nw1,1).reshape(Nodes[2],1) * 1/(k+1)  
-
-        # Relu to Relu 
-
+        w1loss  = np.dot (self.loss.reshape(Nodes[2],1), PREVLEAKY.reshape(1,Nodes[1])) 
+        nw1 = w1loss.reshape(Nodes[2],Nodes[1]) * 1/(k+1)  
+        nb1 = np.sum(nw1,1).reshape(Nodes[2],1) * 1/(k+1)  
         # Relu To Pixels
-        wloss = np.dot (Chain.reshape(1,Nodes[2]), WandB[2].reshape(Nodes[2],Nodes[1])) * drelu           
-        self.nw = np.dot (wloss.reshape(Nodes[1],1), pixels.reshape(1,Nodes[0])) * 1/(k+1)
-        self.nb = np.sum(self.nw.reshape(Nodes[1],Nodes[0]),1).reshape(Nodes[1],1) * 1/(k+1)
-
-    
-        return self.nw,self.nb,self.nw1,self.nb1 # change these to self after adding momentum 
+        wloss = np.dot (self.loss.reshape(1,Nodes[2]), WandB[2].reshape(Nodes[2],Nodes[1])) * DL1Relu           
+        nw = np.dot (wloss.reshape(Nodes[1],1), self.scaledarray.reshape(1,Nodes[0])) * 1/(k+1)
+        nb = np.sum(nw.reshape(Nodes[1],Nodes[0]),1).reshape(Nodes[1],1) * 1/(k+1)
+        #wloss2 = np.dot (self.loss.reshape(1,Nodes[2]), WandB[4].reshape(Nodes[2],Nodes[2])) * DL1Relu           
+        #self.neww = np.dot (wloss2.reshape(Nodes[2],1), self.scaledarray.reshape(1,Nodes[1])) * 1/(k+1)
+        #self.newb = np.sum(self.neww.reshape(Nodes[2],Nodes[1]),1).reshape(Nodes[2],1) * 1/(k+1) 
+        # self.loss dot w2  * drelu  dot  pixels
+        return nw,nb,nw1,nb1#self.neww,self.newb,nw1,nb1 
 
     def GradientDescentWithMomentum(self,mu,lr):#aw,ab,aw1,ab1,mu,lr):#,w,b,w1,b1,nw,nb,nw1,nb1,prevnw,prevnb,prevnw1,prevnb1,alterdw,alterdb,alterdw1,alterdb1,mu,lr):
         
@@ -195,16 +170,18 @@ if __name__ == "__main__":
     
     ################# Model #####################
     
-    Nodes = (784,533,10) #(784,533,356,10)  # Enter node layers here
+    Nodes = (784,533,10) 
+    #Nodes = (784,533,356,10)  # Enter node layers here
 
     nn = Sequential() 
     Prevvalues, Altvalues, NWandB, OptWandB = nn.init_delta(), nn.init_delta(), nn.init_delta(), nn.init_delta()
     WandB = nn.weightsandbiases() # Both of these dynamically grow.
 
     t = 0
-    Images = 10     #29400 # training at 81 percent for 29,400 images. 
+    Images = 100     #29400 # training at 81 percent for 29,400 images. 
     Momentum = 0.9
     Learning_Rate = 0.1
+
 
     for k in range(0,Images): #trainingset:  # loops through images. 90 sec = 10 images image 0 and forward 
         
@@ -213,9 +190,14 @@ if __name__ == "__main__":
         
         L1 = nn.Linear( Nodes[0], Nodes[1], imgs_,WandB[0],WandB[1])
         L1Relu = nn.LeakyRelU(L1,0.01)  
-        nn.D_LeakyRelU(0.01)
-
+        DL1Relu = nn.D_LeakyRelU(L1Relu,0.01)
         ######################### Starting Backward Prop #########################
+
+        #L2 = nn.Linear( Nodes[1], Nodes[2], L1Relu,WandB[2],WandB[3])
+        #L2Relu = nn.LeakyRelU(L2,0.01)  
+        #DL2Relu = nn.D_LeakyRelU(L2Relu,0.01)
+        #L3 = nn.Linear( Nodes[2], Nodes[3], L2Relu, WandB[4],WandB[5])  #! change nodes to 2 and 3 and l1relu to l2relu
+
         L3 = nn.Linear( Nodes[1], Nodes[2], L1Relu, WandB[2],WandB[3])  #! change nodes to 2 and 3 and l1relu to l2relu
 
         nn.Softmax(L3) 
@@ -226,9 +208,11 @@ if __name__ == "__main__":
         softloss = nn.D_CCE_and_Softmax() # Cross entropy & SOFTMAX Chain Rule derivative
 
         if k!=0:
-            Prevvalues = NWandB # store previous weights and biases. 
-            
-        NWandB = nn.Backward_Prop()
+            Prevvalues = NWandB # store previous weights and biases. CHANGE TO PLUS AND =.
+        
+        #test = nn.Leaky_Relu_BackProp(DL2Relu,L2Relu)
+       
+        NWandB = nn.Backward_Prop(L1Relu,DL1Relu)
         WandB, Altvalues = nn.GradientDescentWithMomentum(Momentum,Learning_Rate)    
 
     accuracy = t / Images # len (trainingset) # change to len (testingset) when running testing set.    
